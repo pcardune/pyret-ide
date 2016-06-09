d327b4e6a588757cf08f11ddbbcb728e72279dbb
Paul Carduner <paul@carduner.net>
2016-06-09 15:35:34 -0700

    Integrate Toolbar component into PyretIDE

--- src/components/PyretIDE.js
+++ src/components/PyretIDE.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Toolbar from './Toolbar';
+
 export default class PyretIDE extends React.Component {
   render() {
     return (
       <div>
+        <Toolbar logo="https://code.pyret.org/img/pyret-logo.png"/>
         The Pyret IDE!
       </div>
     );
