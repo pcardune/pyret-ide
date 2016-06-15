ab01a9010f5737ca35ee18607d053221e13f6d4f
Paul Carduner <paul@carduner.net>
2016-06-15 14:23:08 -0700

    Add example test file

---
+++ spec/demo-test.js
@@ -0,0 +1,5 @@
+describe("the test system", () => {
+  it("should work!", () => {
+    expect(1).toBe(1);
+  });
+});
--- spec/index.js
+++ spec/index.js
@@ -4,9 +4,3 @@ require('babel-polyfill');
 // require all the files in the spec folder that end with -test.js
 var context = require.context('.', true, /.*-test.js$/);
 context.keys().forEach(context);
-
-describe("the test system", () => {
-  it("should work!", () => {
-    expect(1).toBe(1);
-  });
-});
