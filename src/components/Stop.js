59c47a309ffdba81dd7f0be4b0358b8429934fb5
Benjamin Limpich <benlimpich@gmail.com>
2016-06-20 16:28:08 -0700

    added a TODO comment

--- src/components/Stop.js
+++ src/components/Stop.js
@@ -1,9 +1,12 @@
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import React from "react";
 import Radium from "radium";
 import Button from "./Button";
 
 class Stop extends React.Component {
   render() {
+    //change to this.props.running (does that mean remaking propdeclerations like in Run.js or importing them somehow or making a seperate file for them and then importing them or what?)
     if (this.state.running) {
       return (
         <Button kind="stop" style={{backgroundColor: "#FF0000"}}>Stop</Button>
@@ -16,4 +19,6 @@ class Stop extends React.Component {
   }
 }
 
-export default Radium(Stop);
+export default connect(
+  
+)(Radium(Stop));
