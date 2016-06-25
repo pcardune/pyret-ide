021333ebc888f949ee1cafb0baa4d23296a53f09
Benjamin Limpich <benlimpich@gmail.com>
2016-06-24 19:19:55 -0700

    Implemented loadRuntimeApi action creator and tests

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,9 +1,29 @@
 import {RUNNING} from './action-types';
+import * as actType from './action-types';
 
 export function run() {
   return {
     type: RUNNING,
     payload: true
+  };
+}
+
+/**
+ * @param function runtimeApiLoader - a function that asynchronously
+ *                                    loads a runtime api. It should return
+ *                                    a promise that resolves to the loaded api.
+ */
+export function loadRuntimeApi(runtimeApiLoader) {
+  return function(dispatch) {
+    dispatch({type: actType.START_LOAD_RUNTIME});
+    var promise = runtimeApiLoader();
+    promise
+      .then(loadedRuntimeApi => {
+        dispatch({type: actType.FINISH_LOAD_RUNTIME, payload: loadedRuntimeApi});
+      })
+      .catch(reason => {
+        dispatch({type: actType.FAIL_LOAD_RUNTIME, payload: reason});
+      });
   };
 }
 
