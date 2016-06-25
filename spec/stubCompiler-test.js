c4b543e0bbf6fc062089235fbd9bb7919d4c6881
Paul Carduner <paul@carduner.net>
2016-06-24 23:33:38 -0700

    make the stub compiler a little more interesting
    Now it will do basic arithmetic given infix notation.  cc @blimpich and
    @farmanrl FYI.

--- spec/stubCompiler-test.js
+++ spec/stubCompiler-test.js
@@ -1,49 +1,67 @@
 import stubCompiler from '../src/stubCompiler';
 
 describe("The stubCompiler's", () => {
-  
+
   describe("parse function", () => {
-  
+
     it("returns a Promise instance", () => {
-      expect(stubCompiler.parse()).toEqual(jasmine.any(Promise)); 
+      expect(stubCompiler.parse()).toEqual(jasmine.any(Promise));
     });
-    
+
     it("returns a Promise that resolves to some fake ast", (done) => {
-      var promise = stubCompiler.parse();
+      var promise = stubCompiler.parse("(13+252)*10");
       promise.then(ast => {
-        expect(ast).toBe('some fake ast');
+        expect(ast).toEqual(['13', '252', '+', '10', '*']);
         done();
+      }).catch(error => {
+        done.fail(error);
       });
     });
   });
 
   describe("compile function", () => {
-    
+
     it("returns a Promise instance", () => {
-      expect(stubCompiler.compile()).toEqual(jasmine.any(Promise)); 
+      expect(stubCompiler.compile()).toEqual(jasmine.any(Promise));
     });
-    
+
     it("returns a Promise that resolves to some fake bytecode", (done) => {
-      var promise = stubCompiler.compile();
+      var promise = stubCompiler.compile(['13', '252', '+', '10', '*']);
       promise.then(bytecode => {
-        expect(bytecode).toBe('some fake bytecode');
+        expect(bytecode).toEqual(jasmine.any(Function));
+        done();
+      }).catch(error => {
+        done.fail(error);
+      });
+    });
+  });
+
+  describe("execute function", () => {
+
+    it("returns a Promise instance", () => {
+      expect(stubCompiler.execute()).toEqual(jasmine.any(Promise));
+    });
+
+    it("returns a Promise that resolves to some fake result", (done) => {
+      var promise = stubCompiler.execute(() => 'some result');
+      promise.then(result => {
+        expect(result).toBe('some result');
         done();
       });
     });
   });
-  
-  describe("execute function", () => {
-    
-    it("returns a Promise instance", () => {
-      expect(stubCompiler.execute()).toEqual(jasmine.any(Promise)); 
-    });
-    
-    it("returns a Promise that resolves to some fake result", (done) => {
-      var promise = stubCompiler.execute();
-      promise.then(result => {
-        expect(result).toBe('some fake result');
-        done();
-      });
+
+  describe("all together now", () => {
+    it("you can combine the parse, compile, and execute functions together", (done) => {
+      stubCompiler
+        .parse("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3")
+        .then(stubCompiler.compile)
+        .then(stubCompiler.execute)
+        .then(result => {
+          expect(result).toEqual(823.125);
+          done();
+        })
+        .catch(error => done.fail(error));
     });
   });
 });
--- src/stubCompiler.js
+++ src/stubCompiler.js
@@ -1,28 +1,97 @@
 import "babel-polyfill";
 
+const OPS = {
+  '-': (a,b) => a() - b(),
+  '+': (a,b) => a() + b(),
+  '/': (a,b) => a() / b(),
+  '*': (a,b) => a() * b(),
+  '^': (a,b) => Math.pow(a(), b()),
+};
+
 export default {
-  
-  parse() {
-    return new Promise((resolve) => {
-      window.setTimeout(() => {
-        resolve('some fake ast');
-      }, 1000);  
+
+  parse(src) {
+    return new Promise((resolve, reject) => {
+      if (!src) {
+        return reject(new Error("no source code provided"));
+      }
+      var infix = src.replace(/\s+/g, ''); // remove spaces, so infix[i]!=" "
+
+      var stack = [];
+      const PRECEDENCE = {"^":4, "*":3, "/":3, "+":2, "-":2};
+      const ASSOCIATIVITY = {"^":"Right", "*":"Left", "/":"Left", "+":"Left", "-":"Left"};
+      var token;
+      var postfix = [];
+      var o1, o2;
+
+      for (var i = 0; i < infix.length; i++) {
+        token = infix[i];
+        if (token >= "0" && token <= "9") {
+          // token is part of an operand
+          var prev = infix[i - 1];
+          if (prev && prev[0] >= "0" && prev[0] <= "9") {
+            postfix[postfix.length - 1] += token;
+          } else {
+            postfix.push(token);
+          }
+        } else if (OPS[token]) {
+          // token is an operator
+          o1 = token;
+          o2 = stack[stack.length - 1];
+          while (OPS[o2] && (
+            // while operator token, o2, on top of the stack
+            // and o1 is left-associative and its precedence is
+            // less than or equal to that of o2
+            (ASSOCIATIVITY[o1] == "Left" && (PRECEDENCE[o1] <= PRECEDENCE[o2])) ||
+            // or o1 is right-associative and its precedence is less than that of o2
+            (ASSOCIATIVITY[o1] == "Right" && (PRECEDENCE[o1] < PRECEDENCE[o2]))
+          )){
+            postfix.push(o2);
+            stack.pop();
+            o2 = stack[stack.length - 1];
+          }
+          stack.push(o1);
+        } else if (token == "(") {
+          stack.push(token);
+        } else if (token == ")") {
+          while (stack[stack.length - 1] != "("){
+            // move token from top of stack to output queue until we hit (
+            postfix.push(stack.pop());
+          }
+          stack.pop(); // pop (, but not onto the output queue
+        }
+      }
+      while (stack.length > 0){
+        postfix.push(stack.pop());
+      }
+
+      resolve(postfix);
     });
   },
-  
-  compile() {
-    return new Promise((resolve) => {
-      window.setTimeout(() => {
-        resolve('some fake bytecode');
-      }, 1000);  
+
+  compile(ast) {
+    return new Promise((resolve, reject) => {
+      if (!ast) {
+        reject(new Error("no ast provided"));
+      }
+      function comp() {
+        var next = ast.pop();
+        if (OPS[next]) {
+          return OPS[next].bind(null, comp(ast), comp(ast));
+        } else {
+          return () => parseInt(next);
+        }
+      }
+      resolve(comp(ast));
     });
   },
-  
-  execute() {
-    return new Promise((resolve) => {
-      window.setTimeout(() => {
-        resolve('some fake result');
-      }, 1000);  
+
+  execute(bytecode) {
+    return new Promise((resolve, reject) => {
+      if (!bytecode) {
+        reject(new Error("No bytecode provided"));
+      }
+      resolve(bytecode());
     });
   }
 };
