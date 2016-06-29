f67579674b166a94fbcc576fb03b85c82711ddc3
Richard Farman <farmanrl@whitman.edu>
2016-06-28 18:07:25 -0700

    Add reducer and tests for runCode, combinedReducer

--- spec/redux/reducer-test.js
+++ spec/redux/reducer-test.js
@@ -1,43 +1,135 @@
-import loadApi from '../../src/redux/reducer';
+import pyretReducer from '../../src/redux/reducer';
 import * as actType from '../../src/redux/action-types';
 
 describe("The reducer", () => {
 
-  it("returns a state object", () => {
-    var nextState = loadApi(undefined,"");
-    expect(nextState).toEqual(jasmine.any(Object));
+  describe("handles loadApi calls", () => {
+
+    it("and returns a state object", () => {
+      var nextState = pyretReducer(undefined,"");
+      expect(nextState).toEqual(jasmine.any(Object));
+    });
+
+    describe("and returns a state change", () => {
+      var state;
+
+      beforeEach(() => {
+        state = {
+          loadingApi: undefined,
+          runtimeApi: undefined,
+          errorApi: undefined,
+          runtimeStage: undefined,
+          ast: undefined,
+          bytecode: undefined,
+          result: undefined,
+          errorRun: undefined,
+          pausing: false
+        };
+      });
+
+      const start = {type: actType.START_LOAD_RUNTIME};
+      const finish = {type: actType.FINISH_LOAD_RUNTIME, payload: "loadedRuntimeApi"};
+      const fail = {type: actType.FAIL_LOAD_RUNTIME, payload: "reason"};
+
+      it("for action type START_LOAD_RUNTIME", () => {
+        var nextState = pyretReducer(state, start).loadApi;
+        expect(nextState.loadingApi).toEqual('starting');
+      });
+
+      it("for action type FINISH_LOAD_RUNTIME", () => {
+        var nextState = pyretReducer(state, finish).loadApi;
+        expect(nextState.loadingApi).toEqual('finished');
+        expect(nextState.runtimeApi).toEqual(finish.payload);
+      });
+
+      it("for action type FAIL_LOAD_RUNTIME", () => {
+        var nextState = pyretReducer(state, fail).loadApi;
+        expect(nextState.loadingApi).toEqual('failed');
+        expect(nextState.error).toEqual(fail.payload);
+      });
+    });
   });
 
-  describe("returns a state change", () => {
-    var state;
+  describe("handles runCode calls", () => {
 
-    beforeEach(() => {
-      state = {
-        loadingApi: undefined,
-        runtimeApi: undefined,
-        error: undefined
-      };
+    it("and returns a state object ", () => {
+      var nextState = pyretReducer(undefined,"");
+      expect(nextState).toEqual(jasmine.any(Object));
     });
 
-    const start = {type: actType.START_LOAD_RUNTIME};
-    const finish = {type: actType.FINISH_LOAD_RUNTIME, payload: "loadedRuntimeApi"};
-    const fail = {type: actType.FAIL_LOAD_RUNTIME, payload: "reason"};
+    describe("and returns a state change", () => {
+      var state;
 
-    it("for action type START_LOAD_RUNTIME", () => {
-      var nextState = loadApi(state, start);
-      expect(nextState.loadingApi).toEqual('started');
-    });
+      beforeEach(() => {
+        state = {
+          loadingApi: undefined,
+          runtimeApi: undefined,
+          errorApi: undefined,
+          runtimeStage: undefined,
+          ast: undefined,
+          bytecode: undefined,
+          result: undefined,
+          errorRun: undefined,
+          pausing: false
+        };
+      });
 
-    it("for action type FINISH_LOAD_RUNTIME", () => {
-      var nextState = loadApi(state, finish);
-      expect(nextState.loadingApi).toEqual('finished');
-      expect(nextState.runtimeApi).toEqual(finish.payload);
-    });
+      const startParse = {type: actType.START_PARSE};
+      const finishParse = {type: actType.FINISH_PARSE, payload: "ast"};
+      const failParse = {type: actType.FAIL_PARSE, payload: "reason"};
 
-    it("for action type FAIL_LOAD_RUNTIME", () => {
-      var nextState = loadApi(state, fail);
-      expect(nextState.loadingApi).toEqual('failed');
-      expect(nextState.error).toEqual(fail.payload);
+      const startCompile = {type: actType.START_COMPILE};
+      const finishCompile = {type: actType.FINISH_COMPILE, payload: "bytecode"};
+      const failCompile = {type: actType.FAIL_COMPILE, payload: "reason"};
+
+      const startExecute = {type: actType.START_EXECUTE};
+      const finishExecute = {type: actType.FINISH_EXECUTE, payload: "result"};
+      const failExecute = {type: actType.FAIL_EXECUTE, payload: "reason"};
+
+      it("for action type START_PARSE", () => {
+        var nextState = pyretReducer(state, startParse).runCode;
+        expect(nextState.runtimeStage).toEqual('parsing');
+      });
+
+      it("for action type FINISH_PARSE", () => {
+        var nextState = pyretReducer(state, finishParse).runCode;
+        expect(nextState.ast).toEqual('ast');
+      });
+
+      it("for action type FAIL_PARSE", () => {
+        var nextState = pyretReducer(state, failParse).runCode;
+        expect(nextState.error).toEqual('reason');
+      });
+
+      it("for action type START_COMPILE", () => {
+        var nextState = pyretReducer(state, startCompile).runCode;
+        expect(nextState.runtimeStage).toEqual('compiling');
+      });
+
+      it("for action type FINISH_COMPILE", () => {
+        var nextState = pyretReducer(state, finishCompile).runCode;
+        expect(nextState.bytecode).toEqual('bytecode');
+      });
+
+      it("for action type FAIL_COMPILE", () => {
+        var nextState = pyretReducer(state, failCompile).runCode;
+        expect(nextState.error).toEqual('reason');
+      });
+
+      it("for action type START_EXECUTE", () => {
+        var nextState = pyretReducer(state, startExecute).runCode;
+        expect(nextState.runtimeStage).toEqual('executing');
+      });
+
+      it("for action type FINISH_EXECUTE", () => {
+        var nextState = pyretReducer(state, finishExecute).runCode;
+        expect(nextState.result).toEqual('result');
+      });
+
+      it("for action type FAIL_EXECUTE", () => {
+        var nextState = pyretReducer(state, failExecute).runCode;
+        expect(nextState.error).toEqual('reason');
+      });
     });
   });
 });
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,22 @@
 import * as actType from './action-types';
+import { combineReducers } from 'redux';
 
-const initialState = {
+const loadApiState = {
   loadingApi: undefined,
   runtimeApi: undefined,
-  error: undefined
+  errorApi: undefined,
 };
 
-function loadApi(state = initialState, action) {
+const runCodeState = {
+  runtimeStage: undefined,
+  ast: undefined,
+  bytecode: undefined,
+  result: undefined,
+  errorRun: undefined,
+  pausing: false
+};
+
+function loadApi(state = loadApiState, action) {
   switch (action.type) {
     case actType.START_LOAD_RUNTIME:
       return Object.assign({}, state, {loadingApi: 'started'});
@@ -19,4 +29,40 @@ function loadApi(state = initialState, action) {
   }
 }
 
-export default loadApi;
+function runCode(state = runCodeState, action) {
+  switch (action.type) {
+    case actType.START_PARSE:
+      return Object.assign({}, state, {runtimeStage: 'parsing'});
+    case actType.FINISH_PARSE:
+      return Object.assign({}, state, {ast: action.payload});
+    case actType.FAIL_PARSE:
+      return Object.assign({}, state, {error: action.payload});
+    case actType.START_COMPILE:
+      return Object.assign({}, state, {runtimeStage: 'compiling'});
+    case actType.FINISH_COMPILE:
+      return Object.assign({}, state, {bytecode: action.payload});
+    case actType.FAIL_COMPILE:
+      return Object.assign({}, state, {error: action.payload});
+    case actType.START_EXECUTE:
+      return Object.assign({}, state, {runtimeStage: 'executing'});
+    case actType.FINISH_EXECUTE:
+      return Object.assign({}, state, {result: action.payload});
+    case actType.FAIL_EXECUTE:
+      return Object.assign({}, state, {error: action.payload});
+    case actType.STOP_RUN:
+      return Object.assign({}, state, {runtimeStage: 'stopping'});
+    case actType.PAUSE_RUN:
+      return Object.assign({}, state, {pausing: true});
+    case actType.CLEAR_STATE:
+      return Object.assign({}, runCodeState);
+    default:
+      return state;
+  }
+}
+
+const pyretReducer = combineReducers({
+  runCode,
+  loadApi
+});
+
+export default pyretReducer;
