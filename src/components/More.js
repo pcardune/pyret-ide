ccdee2e7017b3b9c2d2b30842b1c4e00d8b35757
Benjamin Limpich <benlimpich@gmail.com>
2016-07-11 19:55:13 -0700

    implement more button

--- src/components/More.js
+++ src/components/More.js
@@ -1,18 +1,45 @@
 import React from "react";
 import Radium from "radium";
 import Button from "./Button";
+import {isMoreMenuExpanded} from "../redux/selectors";
+import {expandMoreMenu, collapseMoreMenu} from '../redux/actionCreators';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {styles} from './styles';
 
-//TODO
-//implement correct behavior
 export class More extends React.Component {
   render() {
+    if (this.props.expanded) {
+      return(
+        <Button kind="toolbar"
+                style={styles.buttons.more.moreButton}
+                onClick={()=> this.props.collapse()}>
+          More
+        </Button>
+      );
+    }
     return(
       <Button kind="toolbar"
-              onClick={()=> this.setState({expanded: true})}>
+              style={styles.buttons.more.moreButton}
+              onClick={()=> this.props.expand()}>
         More
       </Button>
     );
   }
 }
 
-export default Radium(More);
+More.propTypes = {
+  expanded: React.PropTypes.bool,
+  expand: React.PropTypes.func,
+  collapse: React.PropTypes.func,
+};
+
+export default connect(
+  state => ({
+    expanded: isMoreMenuExpanded(state),
+  }),
+  dispatch => bindActionCreators( {
+    expand: expandMoreMenu,
+    collapse: collapseMoreMenu,
+  }, dispatch)
+)(Radium(More));
