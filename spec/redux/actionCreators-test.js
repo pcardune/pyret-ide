e6975a719c7aeff8099964112c7a1ddb851154ab
Richard Farman <farmanrl@whitman.edu>
2016-07-19 17:15:02 -0700

    add consistency for ' and "

--- spec/redux/actionCreators-test.js
+++ spec/redux/actionCreators-test.js
@@ -23,7 +23,7 @@ describe("The actionCreators'", () => {
         reject = _reject;
       });
       store.dispatch(actCreators.configureIDE({
-        codemirrorOptions: {mode: "javascript"},
+        codemirrorOptions: {mode: 'javascript'},
         runtimeApiLoader: runtimeApiLoader
       }));
     });
@@ -31,23 +31,23 @@ describe("The actionCreators'", () => {
     describe("asynchronously dispatches the loadRuntimeApi function", () => {
 
       it("which dispatches a START_LOAD_RUNTIME action first", () => {
-        expect(store.getActions()[0]).toEqual({type: "START_LOAD_RUNTIME"});
+        expect(store.getActions()[0]).toEqual({type: 'START_LOAD_RUNTIME'});
       });
 
       it("and dispatches a FINISH_LOAD_RUNTIME after the runtime is loaded", (done) => {
-        resolve("the runtime api");
+        resolve('the runtime api');
         window.setTimeout(() => {
           expect(store.getActions()[2])
-            .toEqual({type: "FINISH_LOAD_RUNTIME", payload: "the runtime api"});
+            .toEqual({type: 'FINISH_LOAD_RUNTIME', payload: 'the runtime api'});
           done();
         }, 0);
       });
 
       it("and dispatches a FAIL_LOAD_RUNTIME after the promise is rejected", (done) => {
-        reject("some error");
+        reject('some error');
         window.setTimeout(() => {
           expect(store.getActions()[2])
-            .toEqual({type: "FAIL_LOAD_RUNTIME", payload: "some error"});
+            .toEqual({type: 'FAIL_LOAD_RUNTIME', payload: 'some error'});
           done();
         }, 0);
       });
@@ -55,7 +55,7 @@ describe("The actionCreators'", () => {
 
     it("dispatches a CONFIGURE_CODEMIRROR action after loading runtime", () => {
       expect(store.getActions()[1]).toEqual({
-        type: "CONFIGURE_CODEMIRROR",
+        type: 'CONFIGURE_CODEMIRROR',
         payload: {mode: 'javascript'}
       });
     });
@@ -65,25 +65,25 @@ describe("The actionCreators'", () => {
 
     describe("changeREPLCode function", () => {
       it("dispatches a CHANGE_REPL_CODE action", () => {
-        store.dispatch(actCreators.changeREPLCode("some code"));
+        store.dispatch(actCreators.changeREPLCode('some code'));
         expect(store.getActions()[0])
-          .toEqual({type: actType.CHANGE_REPL_CODE, payload: "some code"});
+          .toEqual({type: actType.CHANGE_REPL_CODE, payload: 'some code'});
       });
     });
 
     describe("recieveREPLResult function", () => {
       it("dispatches a RECEIVE_REPL_RESULT action", () => {
-        store.dispatch(actCreators.recieveREPLResult("some result"));
+        store.dispatch(actCreators.recieveREPLResult('some result'));
         expect(store.getActions()[0])
-          .toEqual({type: actType.RECEIVE_REPL_RESULT, payload: "some result"});
+          .toEqual({type: actType.RECEIVE_REPL_RESULT, payload: 'some result'});
       });
     });
 
     describe("changeSource function", () => {
       it("dispatches a CHANGE_SOURCE action", () => {
-        store.dispatch(actCreators.changeSource("some code"));
+        store.dispatch(actCreators.changeSource('some code'));
         expect(store.getActions()[0])
-          .toEqual({type: actType.CHANGE_SOURCE, payload: "some code"});
+          .toEqual({type: actType.CHANGE_SOURCE, payload: 'some code'});
       });
 
       describe("clearState function", () => {
@@ -99,11 +99,11 @@ describe("The actionCreators'", () => {
 
     var parseResolve, parseReject, compileResolve, compileReject;
     var executeResolve, executeReject, store;
-    var src = "some source code";
+    var src = 'some source code';
 
     beforeEach(() => {
 
-      let state = pyretReducer(undefined, {type: "START_PARSE"});
+      let state = pyretReducer(undefined, {type: 'START_PARSE'});
       state = pyretReducer(state, {
         type: actType.FINISH_LOAD_RUNTIME,
         payload: {
@@ -140,39 +140,39 @@ describe("The actionCreators'", () => {
 
     it("dispatches a STORE_SOURCE action", () => {
       expect(store.getActions()[0]).toEqual(
-        {type: "STORE_SOURCE", payload: "some source code"}
+        {type: 'STORE_SOURCE', payload: 'some source code'}
       );
     });
 
     describe("asynchronously dispatches the loadRuntimeApi function which", () => {
 
       it("dispatches a START_PARSE action", () => {
-        expect(store.getActions()[1]).toEqual({type: "START_PARSE", stage: 'parsing'});
+        expect(store.getActions()[1]).toEqual({type: 'START_PARSE', stage: 'parsing'});
       });
 
       it("dispatches a FINISH_PARSE action once the source code is parsed", (done) => {
-        parseResolve("the ast");
+        parseResolve('the ast');
         window.setTimeout(() => {
           expect(store.getActions()[2])
-            .toEqual({type: "FINISH_PARSE", payload: "the ast"});
+            .toEqual({type: 'FINISH_PARSE', payload: 'the ast'});
           done();
         }, 0);
       });
 
       it("dispatches a START_COMPILE action once the source code is parsed", (done) => {
-        parseResolve("the src");
+        parseResolve('the src');
         window.setTimeout(() => {
           expect(store.getActions()[3])
-            .toEqual({type: "START_COMPILE", stage: 'compiling'});
+            .toEqual({type: 'START_COMPILE', stage: 'compiling'});
           done();
         }, 0);
       });
 
       it("dispatches a FAIL_PARSE action once the first promise is rejected", (done) => {
-        parseReject("some error");
+        parseReject('some error');
         window.setTimeout(() => {
           expect(store.getActions()[2])
-            .toEqual({type: "FAIL_PARSE", payload: "some error"});
+            .toEqual({type: 'FAIL_PARSE', payload: 'some error'});
           done();
         }, 0);
       });
@@ -180,54 +180,54 @@ describe("The actionCreators'", () => {
       describe("and the compile function,", () => {
 
         beforeEach((done) => {
-          parseResolve("the ast");
+          parseResolve('the ast');
           window.setTimeout(done, 0);
         });
 
         it("dispatches a FINISH_COMPILE action once the ast is compiled", (done) => {
-          compileResolve("the byte code");
+          compileResolve('the byte code');
           window.setTimeout(() => {
             expect(store.getActions()[4])
-              .toEqual({type: "FINISH_COMPILE", payload: 'the byte code'});
+              .toEqual({type: 'FINISH_COMPILE', payload: 'the byte code'});
             done();
           }, 0);
         });
 
         it("dispatches a START_EXECUTE action once the ast is compiled", (done) => {
-          compileResolve("the bytecode");
+          compileResolve('the bytecode');
           window.setTimeout(() => {
             expect(store.getActions()[5])
-              .toEqual({type: "START_EXECUTE", stage: 'executing'});
+              .toEqual({type: 'START_EXECUTE', stage: 'executing'});
             done();
           }, 0);
         });
 
         it("dispatches a FAIL_COMPILE action once the second promise is rejected",
            (done) => {
-             compileReject("some error");
+             compileReject('some error');
              window.setTimeout(() => {
                expect(store.getActions()[4])
-                 .toEqual({type: "FAIL_COMPILE", payload: "some error"});
+                 .toEqual({type: 'FAIL_COMPILE', payload: 'some error'});
                done();
              }, 0);
            });
 
-        describe("and the execute function,", () => {
+        describe("and the execute function", () => {
 
           beforeEach((done) => {
-            compileResolve("the bytecode");
+            compileResolve('the bytecode');
             window.setTimeout(done, 0);
           });
 
           it(`dispatches a FINISH_EXECUTE and STORE_EDITOR_RESULT
               action once the bytecode is executed`,
              (done) => {
-               executeResolve("some result");
+               executeResolve('some result');
                window.setTimeout(() => {
                  expect(store.getActions()[6])
-                   .toEqual({type: "FINISH_EXECUTE", payload: "some result"});
+                   .toEqual({type: 'FINISH_EXECUTE', payload: 'some result'});
                  expect(store.getActions()[7])
-                   .toEqual({type: "STORE_EDITOR_RESULT", payload: "some result"});
+                   .toEqual({type: 'STORE_EDITOR_RESULT', payload: 'some result'});
 
                  done();
                }, 0);
@@ -235,10 +235,10 @@ describe("The actionCreators'", () => {
 
           it("dispatches a FAIL_EXECUTE action once the third promise is rejected",
              (done) => {
-               executeReject("some error");
+               executeReject('some error');
                window.setTimeout(() => {
                  expect(store.getActions()[6])
-                   .toEqual({type: "FAIL_EXECUTE", payload: "some error"});
+                   .toEqual({type: 'FAIL_EXECUTE', payload: 'some error'});
                  done();
                }, 0);
              });
